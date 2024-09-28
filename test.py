0743cc260304b72fea04d7639842951a184ee3b4
Rion Tuscano <riontuscano@gmail.com>
2024-09-28 12:02:43 +0530

    basic nav done

--- test.py
+++ test.py
@@ -3,13 +3,8 @@ from flask import Flask,render_template,redirect
 app=Flask(__name__)
 
 @app.route("/")
-def tp():
-    return render_template("tp.html")
-
-
-@app.route("/admin")
-def admin_panel():
-    return render_template("admin.html")
+def index():
+    return render_template("index.html")
 
 
 if __name__=="__main__":

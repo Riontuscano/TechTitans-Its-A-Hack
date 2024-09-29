6af8e2e69978af962dd269ed6c6c2c02e3df9833
Rion Tuscano <riontuscano@gmail.com>
2024-09-29 11:48:37 +0530

    iframe

--- test.py
+++ test.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/CCtv')
+def CCtv():
     return render_template('CCtv.html')
 
 def generate_frames():

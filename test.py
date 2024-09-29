0786b56744d2d9e4170b9caf26e5d695fcd0c854
Rion Tuscano <riontuscano@gmail.com>
2024-09-29 10:35:55 +0530

    cctv basic done

--- test.py
+++ test.py
@@ -6,6 +6,10 @@ app=Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/CCtv")
+def CCtv():
+    return render_template("CCtv.html")
+
 
 if __name__=="__main__":
     app.run(debug=True)
